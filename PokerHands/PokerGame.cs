531f9a78bde86a0c468a1ccd2e3676ef17392c93
cm-rogers <49442265+cm-rogers@users.noreply.github.com>
2019-05-29 07:19:47 +0100

    WIP: Created projected, started on first test

---
+++ PokerHands/PokerGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace PokerHands
+{
+    public class PokerGame
+    {
+        public static void PlayCards(string hand1, string hand2)
+        {
+            var hand1Cards = hand1.Split(" ")
+                .Select(card => new Card
+                {
+                    Value = card.Substring(0,1)
+                });
+
+            var hand2Cards = hand2.Split(" ");
+        }
+    }
+
+    public class Card
+    {
+        public string Value { get; set; }
+    }
+}
