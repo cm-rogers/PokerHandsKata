1cbbaf98d68f311fc84465408b88263922222d8b
cm-rogers <49442265+cm-rogers@users.noreply.github.com>
2019-06-09 17:55:56 +0100

    Made generating the expected output easier

--- PokerHands/Tests/PokerGameTests.cs
+++ PokerHands/Tests/PokerGameTests.cs
@@ -18,11 +18,7 @@ namespace PokerHands.Tests
         {
             var player1 = new Player {Name = "Bob", Hand = "2H 3D 5S 9C KD"};
             var player2 = new Player {Name = "Harry", Hand = "2C 3H 4S 8C AH"};
-
-            var player2Hand = new PlayerHand(player2);
-            var expectedOutput = $"Player {player2.Name} wins with a "
-                                 + $"{player2Hand.Best.Type.ToString()}"
-                                 + $" score of {player2Hand.Best.Score}";
+            var expectedOutput = GenerateExpectedOutput(player2);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
@@ -34,15 +30,20 @@ namespace PokerHands.Tests
         {
             var player1 = new Player {Name = "Alice", Hand = "2H 3D 5S KC KD"};
             var player2 = new Player {Name = "Frank", Hand = "2C 3H 8S 8C AH"};
-
-            var player1Hand = new PlayerHand(player1);
-            var expectedOutput = $"Player {player1.Name} wins with a "
-                                 + $"{player1Hand.Best.Type.ToString()}"
-                                 + $" score of {player1Hand.Best.Score}";
+            var expectedOutput = GenerateExpectedOutput(player1);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
             response.Should().Be(expectedOutput);
+        }
+
+        private static string GenerateExpectedOutput(Player player)
+        {
+            var playerHand = new PlayerHand(player);
+
+            return $"Player {player.Name} wins with a "
+                + $"{playerHand.Best.Type.ToString()}"
+                + $" score of {playerHand.Best.Score}";
         }
     }
 }
