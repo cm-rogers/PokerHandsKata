4fb6850d4d62ffdf3ac6a4e077f013e29c85e58e
cm-rogers <49442265+cm-rogers@users.noreply.github.com>
2019-06-21 14:56:30 +0100

    Refactored some tests into theories to show each player winning the given hand type

--- PokerHands/Tests/PokerGameTests.cs
+++ PokerHands/Tests/PokerGameTests.cs
@@ -8,50 +8,74 @@ namespace PokerHands.Tests
     {
         private readonly PokerGame _pokerGame;
 
+        private const string P1Name = "Alice";
+        private const string P2Name = "Frank";
+
         public PokerGameTests()
         {
             _pokerGame = new PokerGame();
         }
 
-        [Fact]
-        public void ReturnsTheNameOfThePlayerWhoWonHighCard()
+        [Theory]
+        [InlineData("2H 3D 5S 9C KD", "2C JH 4S 8C QH", P1Name, 13)]
+        [InlineData("2H 3D 5S 9C KD", "2C 3H 4S 8C AH", P2Name, 14)]
+        public void ReturnsTheNameOfThePlayerWhoWonHighCard(
+            string p1Hand,
+            string p2Hand,
+            string expectedWinnerName,
+            int expectedWinnerScore
+        )
         {
-            var player1 = new Player {Name = "Bob", Hand = "2H 3D 5S 9C KD"};
-            var player2 = new Player {Name = "Harry", Hand = "2C 3H 4S 8C AH"};
+            var player1 = new Player {Name = P1Name, Hand = p1Hand};
+            var player2 = new Player {Name = P2Name, Hand = p2Hand};
             var expectedOutput = GenerateOutputForExpectedWinner(
-                player2.Name,
+                expectedWinnerName,
                 Hand.Types.HighCard,
-                14);
+                expectedWinnerScore);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
             response.Should().Be(expectedOutput);
         }
 
-        [Fact]
-        public void ReturnsTheNameOfThePlayerWhoWonAPair()
+        [Theory]
+        [InlineData("2H 3D 5S 9C 5D", "2C JH 2S 8C QH", P1Name, 10)]
+        [InlineData("2H KD 5S 9C KD", "2C AH 4S 8C AH", P2Name, 28)]
+        public void ReturnsTheNameOfThePlayerWhoWonAPair(
+            string p1Hand,
+            string p2Hand,
+            string expectedWinnerName,
+            int expectedWinnerScore
+        )
         {
-            var player1 = new Player {Name = "Alice", Hand = "2H 3D 5S KC KD"};
-            var player2 = new Player {Name = "Frank", Hand = "2C 3H 8S 8C AH"};
+            var player1 = new Player { Name = P1Name, Hand = p1Hand };
+            var player2 = new Player { Name = P2Name, Hand = p2Hand };
             var expectedOutput = GenerateOutputForExpectedWinner(
-                player1.Name,
+                expectedWinnerName,
                 Hand.Types.Pair,
-                26);
+                expectedWinnerScore);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
             response.Should().Be(expectedOutput);
         }
 
-        [Fact]
-        public void ReturnsTheNameOfThePlayerWhoWonTwoPair()
+        [Theory]
+        [InlineData("3H 9D 5S 9C 3D", "2C 5H 2S 9C 5S", P1Name, 24)]
+        [InlineData("2H 3D 4S 4C KD", "2C AH KS KC AH", P2Name, 54)]
+        public void ReturnsTheNameOfThePlayerWhoWonTwoPair(
+            string p1Hand,
+            string p2Hand,
+            string expectedWinnerName,
+            int expectedWinnerScore
+        )
         {
-            var player1 = new Player {Name = "Alice", Hand = "2H 2D 5S 5C KD"};
-            var player2 = new Player {Name = "Frank", Hand = "2C 3H 8S 8C AH"};
+            var player1 = new Player { Name = P1Name, Hand = p1Hand };
+            var player2 = new Player { Name = P2Name, Hand = p2Hand };
             var expectedOutput = GenerateOutputForExpectedWinner(
-                player1.Name,
+                expectedWinnerName,
                 Hand.Types.TwoPair,
-                14);
+                expectedWinnerScore);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
