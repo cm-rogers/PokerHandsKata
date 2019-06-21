796b143b5ba3439aceadd67c5eb8ebef3f761f1c
cm-rogers <49442265+cm-rogers@users.noreply.github.com>
2019-06-21 14:07:27 +0100

    Refactored the poker game tests to be more specific about the expected outcome string

--- PokerHands/Tests/PokerGameTests.cs
+++ PokerHands/Tests/PokerGameTests.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using PokerHands.Models;
-using PokerHands.Services;
 using Xunit;
 
 namespace PokerHands.Tests
@@ -19,7 +18,7 @@ namespace PokerHands.Tests
         {
             var player1 = new Player {Name = "Bob", Hand = "2H 3D 5S 9C KD"};
             var player2 = new Player {Name = "Harry", Hand = "2C 3H 4S 8C AH"};
-            var expectedOutput = GenerateOutputForExpectedWinner(player2);
+            var expectedOutput = GenerateOutputForExpectedWinner(player2.Name, Hand.Types.HighCard, 14);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
@@ -31,7 +30,7 @@ namespace PokerHands.Tests
         {
             var player1 = new Player {Name = "Alice", Hand = "2H 3D 5S KC KD"};
             var player2 = new Player {Name = "Frank", Hand = "2C 3H 8S 8C AH"};
-            var expectedOutput = GenerateOutputForExpectedWinner(player1);
+            var expectedOutput = GenerateOutputForExpectedWinner(player1.Name, Hand.Types.Pair, 26);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
@@ -43,20 +42,22 @@ namespace PokerHands.Tests
         {
             var player1 = new Player {Name = "Alice", Hand = "2H 3D 5S QC KD"};
             var player2 = new Player {Name = "Frank", Hand = "2C 3H 5S AC AH"};
-            var expectedOutput = GenerateOutputForExpectedWinner(player2);
+            var expectedOutput = GenerateOutputForExpectedWinner(player2.Name, Hand.Types.Pair, 28);
 
             var response = _pokerGame.PlayCards(player1, player2);
 
             response.Should().Be(expectedOutput);
         }
 
-        private static string GenerateOutputForExpectedWinner(Player player)
+        private static string GenerateOutputForExpectedWinner(
+            string playerName,
+            Hand.Types handType,
+            int handScore
+        )
         {
-            var playerHand = new PlayerHand(player);
-
-            return $"Player {player.Name} wins with a "
-                   + $"{playerHand.Best.Type.ToString()}"
-                   + $" score of {playerHand.Best.Score}";
+            return $"Player {playerName} wins with a "
+                   + $"{handType.ToString()}"
+                   + $" score of {handScore}";
         }
     }
 }
